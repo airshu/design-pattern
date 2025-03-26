1fc362ae3452a2136e8ff666617cbf77dda06074
longsj <longsj@xiaopeng.com>
2025-03-26 18:24:53 +0800

    feat 二分查找

---
+++ src/com/shjlone/search/BinarySearch.java
@@ -0,0 +1,32 @@
+package com.shjlone.search;
+
+/**
+ * 二分查找
+ */
+public class BinarySearch {
+
+
+    public static int search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        int target = 6;
+        int index = search(arr, target);
+        System.out.println("index: " + index);
+    }
+}
